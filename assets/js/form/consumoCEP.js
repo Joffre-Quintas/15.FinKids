4f2d3a40b5e0af74a341d57e2594a16c18d2f9cc
Joffre Quintas <112725262+joffrequintas@users.noreply.github.com>
2023-02-25 12:48:10 -0300

    Deletado os arquivos de reset referencia. Criado o setor js

---
+++ assets/js/form/consumoCEP.js
@@ -0,0 +1,35 @@
+const inputCEP = document.querySelector('#cep')
+
+function replaceValue(convertCEP) {
+    const inputAdress = document.querySelector('#adress')
+
+    inputAdress.value = convertCEP.logradouro
+}
+
+async function findCEP(input) {
+    const cep = input.target.value
+    if(cep.trim() == '') {
+        alert('Preencha todos os campos')
+        // inputCEP.style.outline = '2px solid red'
+    } else {
+        try {
+            const correiosAPI = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            const convertCEP = await correiosAPI.json()
+            console.log(convertCEP)
+            if(convertCEP.erro) {
+                document.querySelector('#adress').placeholder = ''
+                document.querySelector('#adress').value = ''
+                alert('CEP não encontrado')
+            }else {
+                replaceValue(convertCEP)
+            }
+        }
+        catch(e) {
+
+        }
+    }
+}
+
+inputCEP.addEventListener('blur', (input)=> {
+    findCEP(input)
+})
